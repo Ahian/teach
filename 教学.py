7946bb16fe7dd0b4d386e572317f1d030d2060b5
ahian <190030514@qq.com>
2018-11-23 11:06:07 +0800

    逻辑判断

--- 教学.py
+++ 教学.py
@@ -5,9 +5,9 @@ a2 = "a1的值:{}".format(a1)
 a3 = True
 
 # 数据结构
-# 数组
+# 数组 有顺序的一组变量
 array = [a1, a2, a3]
-# dict字典
+# dict字典 和数组的核心区别是键值对
 d1={
     "key1": a1,
     "key2": a2,
@@ -16,10 +16,36 @@ d1={
 
 
 def main():
-    print(array[1])
-    print(d1["key2"])
+    # 逻辑判断
+    dic_compare = {
+        "等于": "==",
+        "不等于":"!=",
+        "大于等于":">=",
+    }
+    dic_logic = {
+        "或": 'or',
+        "并且": "and",
+        "非":"not",
+    }
+    weather = 'sunny'
+
+    if weather == 'sunny':
+        print('running')
+    else:
+        print('no running')
+
+    if 1:
+        print('你是傻逼')
 
 
+
+    price=8
+    if price <5:
+        print('买俩瓶')
+    elif price>5 and price>=8 :
+        print('买一瓶')
+    else:
+        print('不买')
 # 程序的入口
 if __name__ == '__main__':
     main()
