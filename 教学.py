9583c1bb4d0784c19c71fe5047548fb5877adbe1
ahian <190030514@qq.com>
2018-11-23 10:22:29 +0800

    增加数据结构

--- 教学.py
+++ 教学.py
@@ -1,11 +1,22 @@
 # 变量
 a1 = 1
-a2 = "a1的值:{}".format(a1)
+a2 = "a1的值:{}".format()
+# 布尔值真假，本质是1和0
+a3 = True
+
+# 数据结构
+# 数组
+array = [a1, a2, a3]
+# dict字典
+a1={
+    "key1": a1,
+    "key2": a2,
+    "key3": a3,
+}
+
 
 def main():
-    for i in range(9999):
-        a1 = a1 * 7
-        print(a2)
+    print()
 
 # 程序的入口
 if __name__ == '__main__':
