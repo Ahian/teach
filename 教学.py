337de307fe6dc0fdb51d3ea780e78e2d58d5c8ab
ahian <190030514@qq.com>
2018-11-23 10:39:36 +0800

    从数据结构里面如何访问里面的变量

--- 教学.py
+++ 教学.py
@@ -1,6 +1,6 @@
 # 变量
 a1 = 1
-a2 = "a1的值:{}".format()
+a2 = "a1的值:{}".format(a1)
 # 布尔值真假，本质是1和0
 a3 = True
 
@@ -8,7 +8,7 @@ a3 = True
 # 数组
 array = [a1, a2, a3]
 # dict字典
-a1={
+d1={
     "key1": a1,
     "key2": a2,
     "key3": a3,
@@ -16,7 +16,9 @@ a1={
 
 
 def main():
-    print()
+    print(array[1])
+    print(d1["key2"])
+
 
 # 程序的入口
 if __name__ == '__main__':
